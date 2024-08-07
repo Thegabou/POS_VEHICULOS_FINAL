39768b99b1f3e33732e18e915293bfdc8803ff4a
Thegabou <137091443+Thegabou@users.noreply.github.com>
2024-08-07 01:56:16 -0500

    XDDD

--- resources/views/view-users/dashboard_gerente.blade.php
+++ resources/views/view-users/dashboard_gerente.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Dashboard-Gerente</title>
     <link rel="shortcut icon" href="{{asset('imagenes/logo_barra.png')}}" type="image/png">
     <link rel="apple-touch-icon" href="{{asset('imagenes/logo_barra.png')}}">
